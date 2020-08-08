13082aa094910422d0a6f9f122780d9f21bd23c9
Sergio Monsalve <smonsalve@Sergios-MacBook-Pro.local>
2020-08-08 12:37:34 -0500

    Tabs

--- titanic_dashboard/app.py
+++ titanic_dashboard/app.py
@@ -6,6 +6,8 @@ print(plotly.__version__)
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
+from dash.dependencies import Input, Output
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -33,6 +35,20 @@ fig = go.Figure(data=[go.Pie(
                 )
 
 
+
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return dcc.Graph(
+                id='example-graph',
+                figure=fig
+            )
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
+
 app.layout = html.Div(children=[
     html.H1(children='Titanic Dashboard'),
     html.H2(children='by Sergio Monsalve'),
@@ -41,11 +57,22 @@ app.layout = html.Div(children=[
         Trying dash on Titanic Kaggle Data
     '''),
 
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    )
+    dcc.Tabs(id="tabs", value='tab-1', children=[
+        dcc.Tab(label='Pie Chart', value='tab-1'),
+        dcc.Tab(label='Tab two', value='tab-2'),
+    ]),
+
+    html.Div(id='tabs-content'),
+
 ])
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+
+
+
+
